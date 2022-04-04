f2b44181ab31f5b055f63efcd5b8be4da5d43e56
Brandon Miller <bmil_21@yahoo.com>
2022-04-04 16:27:26 -0400

    add tests for snake & regular drafts

--- src/utilities/pick-calculator.test.tsx
+++ src/utilities/pick-calculator.test.tsx
@@ -19,7 +19,6 @@ it('should get picks for snake draft', () => {
   regSpy.mockRestore();
 });
 
-
 it('should get picks for regular draft', () => {
   const snakeSpy = jest.spyOn(pickCalculator, 'getPicksForSnake');
   const regSpy = jest.spyOn(pickCalculator, 'getPicksForRegular');
@@ -32,7 +31,30 @@ it('should get picks for regular draft', () => {
   regSpy.mockRestore();
 });
 
-
 // Get Picks for Snake
 
+it('should return SNAKE draft picks', () => {
+  const expectedOutput = [7, 22, 35, 50, 63, 78, 91, 106, 119, 134, 147, 162, 175, 190, 203, 218,];
+  const picks = pickCalculator.getPicksForSnake(7, 14, 16);
+  expect(picks).toEqual(expectedOutput);
+});
+
+it('should return SNAKE draft picks (2nd pick, 8 teams, 10 rounds)', () => {
+  const expectedOutput = [2, 15, 18, 31, 34, 47, 50, 63, 66, 79,];
+  const picks = pickCalculator.getPicksForSnake(2, 8, 10);
+  expect(picks).toEqual(expectedOutput);
+});
+
 // Get Picks for Regular
+
+it('should return REGULAR draft picks', () => {
+  const expectedOutput = [7, 21, 35, 49, 63, 77, 91, 105, 119, 133, 147, 161, 175, 189, 203, 217,];
+  const picks = pickCalculator.getPicksForRegular(7, 14, 16);
+  expect(picks).toEqual(expectedOutput);
+});
+
+it('should return REGULAR draft picks (2nd pick, 14 teams, 8 rounds)', () => {
+  const expectedOutput = [2, 16, 30, 44, 58, 72, 86, 100,];
+  const picks = pickCalculator.getPicksForRegular(2, 14, 8);
+  expect(picks).toEqual(expectedOutput);
+});
